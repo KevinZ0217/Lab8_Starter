3a97c67487bb79e845d8f5f94db3f4abe8f58dcb
Kevin Zhang <jiz100@ucsd.edu>
2022-11-20 16:30:52 -0800

    finish

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Lab 8 - Starter
+Neelay Joglekar, Kevin Zhang
 1. The first one should be the right answer. We don't need to run the test manually if they are in the github action, and most bugs will be caught before the code are integrated into the repo. 
 2. This is incorrect. We should use unit testing for single function, and use E2E testing to examine the performance of the entire application as opposed to a single function. 
+3. This is incorrect. Sending a message involves multiple moving parts, because multiple users and features will be involved. Thus it's better to use UI testing. 
+4. This is correct. Checking the max length of a message is a small and isolated feature, so it won't interact with other features. Thus, unit test works well. 
+   
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,92 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+//phone number
+//test1
+test('check 111-111-1111 is phone number', () => {
+    expect(functions.isPhoneNumber("111-111-1111")).toBe(true);
+});
+//test2
+test('check (111) 111-1111 is phone number', () => {
+    expect(functions.isPhoneNumber("(111) 111-1111")).toBe(true);
+});
+//test3
+test('check (111)1111111 is not phone number', () => {
+    expect(functions.isPhoneNumber("(111)1111111")).toBe(false);
+});
+//test4
+test('check (111)111-111 is not phone number', () => {
+    expect(functions.isPhoneNumber("(111)111-111")).toBe(false);
+});1
+
+//email
+//test1
+test('check njogleka@ucsd.edu is email', () => {
+    expect(functions.isEmail("njogleka@ucsd.edu")).toBe(true);
+});
+//test2
+test('check jiz100@ucsd.io is email', () => {
+    expect(functions.isEmail("jiz100@ucsd.io")).toBe(true);
+});
+//test3
+test('check njogleka@ucsd.e is not an email', () => {
+    expect(functions.isEmail("njogleka@ucsd.e")).toBe(false);
+});
+//test4
+test('check @ucsd.edu is not an email', () => {
+    expect(functions.isEmail("@ucsd.edu")).toBe(false);
+});
+
+//password
+//test1
+test('check neelay is a password', () => {
+    expect(functions.isStrongPassword("neelay")).toBe(true);
+});
+//test2
+test('check KEVIN is a password', () => {
+    expect(functions.isStrongPassword("KEVIN")).toBe(true);
+});
+//test3
+test('check 666KEVIN is not a password', () => {
+    expect(functions.isStrongPassword("666KEVIN")).toBe(false);
+});
+//test4
+test('check KEVINnnnnnnnnnnnnnnnn is not a password', () => {
+    expect(functions.isStrongPassword("KEVINnnnnnnnnnnnnnnnn")).toBe(false);
+});
+
+//check date
+//test1
+test('check 11/20/2022 is a date', () => {
+    expect(functions.isDate("11/20/2022")).toBe(true);
+});
+//test2
+test('check 1/2/1842 is a date', () => {
+    expect(functions.isDate("1/2/1842")).toBe(true);
+});
+//test3
+test('check 01201842 is not a date', () => {
+    expect(functions.isDate("01201842")).toBe(false);
+});
+//test4
+test('check 11/20/202a is not a date', () => {
+    expect(functions.isDate("11/20/202a")).toBe(false);
+});
+
+//check color
+//test1
+test('check #256 is a hexColor', () => {
+    expect(functions.isHexColor("#256")).toBe(true);
+});
+//test2
+test('check #AaAaAa is a hexColor', () => {
+    expect(functions.isHexColor("#AaAaAa")).toBe(true);
+});
+//test3
+test('check AAA#AAA is not a hexColor', () => {
+    expect(functions.isHexColor("AAA#AAA")).toBe(false);
+});
+//test4
+test('check #ZZZZZZ is not a hexColor', () => {
+    expect(functions.isHexColor("#ZZZZZZ")).toBe(false);
+});
